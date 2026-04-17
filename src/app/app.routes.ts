a63be23477659a8349efd9e0552b66d10d979ade
Luis Garcia <lujogarpin78@gmail.com>
2026-04-17 06:30:03 -0500

    modulos contables

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,15 @@ import { LogsComponent } from './components/logs/logs.component';
 import { SheetsComponent } from './components/sheets/sheets.component';
 import { UploadComponent } from './components/upload/upload.component';
 
+import { ContableComponent } from './components/contable/contable.component';
+import { PlanillaComponent } from './components/planilla/planilla.component';
+
 export const routes: Routes = [
     { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'logs', component: LogsComponent },
     { path: 'sheets', component: SheetsComponent },
     { path: 'upload', component: UploadComponent },
+    { path: 'contable', component: ContableComponent },
+    { path: 'planilla', component: PlanillaComponent },
 ];
